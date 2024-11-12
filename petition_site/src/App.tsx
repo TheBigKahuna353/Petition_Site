05cdecdc3f355b9b103ea975250b6730e77e035f
Jordan <jordan20022@gmail.com>
2024-11-12 14:38:11 +1300

    test

--- petition_site/src/App.tsx
+++ petition_site/src/App.tsx
@@ -14,6 +14,7 @@ import EditPetition from './pages/EditPetition';
 function App() {
   return (
     <div className="App">
+      <p>Teestuibng</p>
       <Router basename={process.env.PUBLIC_URL}>
         <div>
           <Routes>
